c719217f0cfbc6cb7ad6004a25925722dc913a27
Hyperion4ik <Hyperion4ik@gmail.com>
2018-03-15 21:49:32 +0300

    auto

--- sys/netpfil/pf/pf_ioctl.c
+++ sys/netpfil/pf/pf_ioctl.c
@@ -1722,6 +1722,11 @@ relock_DIOCKILLSTATES:
 		struct pf_state		*s;
 		struct pfioc_state_change	*psc = (struct pfioc_state_change *)addr;
 
+		struct pf_state_key	*sk;
+		struct pf_addr		*srcaddr, *dstaddr;
+		u_int16_t		 srcport, dstport;
+		u_int			 i, changed = 0;
+
 		if (psc->psc_pfcmp.id) {
 			if (psc->psc_pfcmp.creatorid == 0)
 				psc->psc_pfcmp.creatorid = V_pf_status.hostid;
@@ -1733,19 +1738,59 @@ relock_DIOCKILLSTATES:
 			break;
 		}
 
-			u_int i;
-
 		for (i = 0; i <= pf_hashmask; i++) {
 			struct pf_idhash *ih = &V_pf_idhash[i];
-relock_xxx:
+
+relock_DIOCCHANGESTATES:
 			PF_HASHROW_LOCK(ih);
 			LIST_FOREACH(s, &ih->states, entry) {
-				printf("state: %lu", s->id);
-				goto relock_xxx;
+				sk = s->key[PF_SK_WIRE];
+				if (s->direction == PF_OUT) {
+					srcaddr = &sk->addr[1];
+					dstaddr = &sk->addr[0];
+					srcport = sk->port[1];
+					dstport = sk->port[0];
+				} else {
+					srcaddr = &sk->addr[0];
+					dstaddr = &sk->addr[1];
+					srcport = sk->port[0];
+					dstport = sk->port[1];
+				}
+
+				if ((!psc->psc_af || sk->af == psc->psc_af)
+				    && (!psc->psc_proto || psc->psc_proto ==
+				    sk->proto) &&
+				    PF_MATCHA(psc->psc_src.neg,
+				    &psc->psc_src.addr.v.a.addr,
+				    &psc->psc_src.addr.v.a.mask,
+				    srcaddr, sk->af) &&
+				    PF_MATCHA(psc->psc_dst.neg,
+				    &psc->psc_dst.addr.v.a.addr,
+				    &psc->psc_dst.addr.v.a.mask,
+				    dstaddr, sk->af) &&
+				    (psc->psc_src.port_op == 0 ||
+				    pf_match_port(psc->psc_src.port_op,
+				    psc->psc_src.port[0], psc->psc_src.port[1],
+				    srcport)) &&
+				    (psc->psc_dst.port_op == 0 ||
+				    pf_match_port(psc->psc_dst.port_op,
+				    psc->psc_dst.port[0], psc->psc_dst.port[1],
+				    dstport)) &&
+				    (!psc->psc_label[0] ||
+				    (s->rule.ptr->label[0] &&
+				    !strcmp(psc->psc_label,
+				    s->rule.ptr->label))) &&
+				    (!psc->psc_ifname[0] ||
+				    !strcmp(psc->psc_ifname,
+				    s->kif->pfik_name))) {
+					//pf_unlink_state(s, PF_ENTER_LOCKED);
+					changed++;
+					goto relock_DIOCKILLSTATES;
+				}
 			}
 			PF_HASHROW_UNLOCK(ih);
 		}
-
+		psc->psc_changed = changed;
 		break;
 	}
 /* SKYNICK */
